24c3db985ec5838ac11c99911811dcf25c6c6082
raghavapollo7 <raghav.rocks453@gmail.com>
2025-11-20 18:39:58 +0530

    Initial commit

---
+++ p1.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter a number:"))
+num2=int(input("Enter a number:"))
+s=num1+num2
+d=num1-num2
+p=num1*num2
+q=num1/num2
+print("Addition:",s)
+print("Subtraction:",d)
+print("Multiplication:",p)
+print("Division:",q)
